fff2fcc5cf3f4b2e340d3e377ef1d984df25bfcb
Hendren <hendren.alex@gmail.com>
2013-04-11 22:09:37 -0400

    stack complete

--- Modules/stack.v
+++ Modules/stack.v
@@ -11,7 +11,7 @@
 //		Parameterized stack with synchronous active low clear
 //
 //////////////////////////////////////////////////////////////////////////////////
-module stack(en, clr, clk, con, data_in, data_out, full);
+module stack(en, clr, clk, con, data_in, data_out);
 	parameter width = 8; //the width of the data in bits
 	parameter depth = 2; // amout of data
 	input en; 	// enable 
@@ -19,14 +19,14 @@ module stack(en, clr, clk, con, data_in, data_out, full);
 	input clk;	// the clock
 	input con;	// controller signal. pop when con=1. push when con=0;
 	input [width-1:0] data_in;
-	output reg full; // inform controller if stack full. 1 on full, else 0
+	reg full; // inform controller if stack full. 1 on full, else 0
 	output reg [width-1:0] data_out;
-		
+	
+	reg [(2**depth)-1:0] size;   // the amount of data in the stack at any given time
 	reg empty; 		// 1 if the stack is empty, else 0
 	reg [width-1:0] data [(2**depth)-1:0]; // reg to hold the data
 	
-	integer i;
-	integer size; // the amount of data in the stack at any given time
+	integer i; 
 	
 	always@(posedge clk)
 	begin
@@ -38,7 +38,8 @@ module stack(en, clr, clk, con, data_in, data_out, full);
 			end
 			full <= 0;
 			empty <= 1;
-			size = 0;
+			size <= 0;
+			data_out <= 0;
 		end
 		else
 		begin
@@ -47,27 +48,29 @@ module stack(en, clr, clk, con, data_in, data_out, full);
 				if (con == 0) // if controller says push 
 				begin
 					if( full == 0 ) //if stack not full
-					begin
-						data[size] <= data_in; // store data
-						size = size + 1; // increment
-						empty <= 0;
-						if(size == (2**depth))
+					begin 
+						if(size < (2**depth))
 						begin
-							full <= 1; // controller must pop to make room
+							data[size] <= data_in; // store data
+							empty <= 0;
+							size <= size + 1; // increment
 						end
+						else
+							full <= 1; // stack is full - controller must pop to make room
 					end
 				end
 				else // pop --> controller = 1 
 				begin
 					if(empty == 0) //if the stack is not empty
 					begin
-						data_out <= data[size]; // output last data in
-						size = size - 1; // decrement
-						full <= 0;
-						if(size == 0)
+						if(size > 0)
 						begin
-							empty <= 1; //stack is empty
+							data_out <= data[size-1]; // output last data in
+							full <= 0;
+							size <= size - 1; // decrement
 						end
+						else
+							empty <= 1; //stack is empty
 					end
 				end
 			end
