3758f6980059147879f9f926c7793ec6715afb89
wakeuky <hendren.alex@gmail.com>
2013-04-16 14:47:56 -0400

    Stage1 Here we go

---
+++ Modules/statge1/stage1.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:47:27 04/16/2013 
+// Design Name: 
+// Module Name:    stage1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module stage1();
+
+
+endmodule
