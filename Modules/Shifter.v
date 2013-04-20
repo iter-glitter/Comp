9a8280aece38906faeaa7264f1c7b9b3aeff7823
Hendren <hendren.alex@gmail.com>
2013-04-20 12:27:00 -0400

    Shifter shifts on same clk cycle, diagram updates,

--- Modules/Shifter.v
+++ Modules/Shifter.v
@@ -1,13 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Engineer:	Zach S McFeely - zachary.mcfeely@uky.edu
+// Alex Hendren
+// Sean McFeely
+// EE480 - Spring 2013 - Heath
+// Accumulator Based Processor
 //
-// Create Date:    18:21:39 02/18/2013 
-// Design Name: 
-// Module Name:    LdStr_shift_Reg_param 
-// Project Name: 
-//
-//
+//	Shifter
 //		Priority (highest lowest): clr, set, clk 
 //			set and clr are synchronous, and active low
 //			
@@ -18,7 +16,7 @@
 //			1		1	 |	Right Shift			
 //
 //////////////////////////////////////////////////////////////////////////////////
-module LdStr_shifter(Reg_in,clr,set,clk,Ls,Rs,ctrl,num_shift,Reg_out);
+module LdStr_shift_Reg_param(Reg_in,clr,set,clk,Ls,Rs,ctrl,num_shift,Reg_out);
 	parameter n = 8; //8-bit accum reg + *Note - if change must change num_shift manually
 	input clr,set,clk,Ls,Rs;	// Ls = Left Shift bit. Rs = Right Shift bit. (1 or 0)
 	input [1:0] ctrl;				// control signal
@@ -29,40 +27,45 @@ module LdStr_shifter(Reg_in,clr,set,clk,Ls,Rs,ctrl,num_shift,Reg_out);
 	reg curr, prev;
 	
 	integer i, j;
-	always@(posedge clk) begin
+	always@(posedge clk, ctrl) begin
 		if(clr == 1'b0) begin	// Clear all bits to zero
-			Reg_out <= 8'b00000000;
+			Reg_out = 8'b00000000;
 		end
 		else if(set == 1'b0) begin // Set all bits to one
-			Reg_out <= 8'b11111111;
+			Reg_out = 8'b11111111;
 		end
-		else if(ctrl == 2'b00) begin //Keep value stored in register
-				Reg_out <= Reg_out;
-		end
-		else if(ctrl == 2'b01) begin //Load in parallel
-				Reg_out <= Reg_in;
-		end
-		else if(ctrl == 2'b10) begin //Left Shift
-			for(i=0; i<num_shift; i=i+1) begin
-				prev <= Reg_out[0];
-				Reg_out[0] <= Ls; 
-				for(j=1;j<n;j=j+1) begin
-						curr <= Reg_out[j]; //save current value
-						Reg_out[j] <= prev; //set current position value to previous position value
-						prev <= curr;
-				end
-			end
-		end
-		else if(ctrl == 2'b11) begin //Right Shift 
-			for(i=0; i<num_shift; i=i+1) begin
-				prev <= Reg_out[n-1];
-				Reg_out[n-1] <= Rs; 
-				for(j=(n-2);j>=0;j=j-1) begin
-						curr <= Reg_out[j]; //save current value
-						Reg_out[j] <= prev; //set current position value to previous position value
-						prev <= curr;
-				end
-			end
+		else begin
+			case(ctrl)
+				2'b00:begin //Keep value stored in register
+						Reg_out = Reg_out;
+						end
+				2'b01:begin //Load in parallel
+						Reg_out = Reg_in;
+						end
+				2'b10:begin //Left Shift
+							for(i=0; i<num_shift; i=i+1) begin
+								prev = Reg_out[0];
+								Reg_out[0] = Ls; 
+								for(j=1;j<n;j=j+1) begin
+										curr = Reg_out[j]; //save current value
+										Reg_out[j] = prev; //set current position value to previous position value
+										prev = curr;
+								end
+							end
+						end
+				2'b11:begin //Right Shift 
+							for(i=0; i<num_shift; i=i+1) begin
+								prev = Reg_out[n-1];
+								Reg_out[n-1] = Rs; 
+								for(j=(n-2);j>=0;j=j-1) begin
+										curr = Reg_out[j]; //save current value
+										Reg_out[j] = prev; //set current position value to previous position value
+										prev = curr;
+								end
+							end
+						end
+				default: Reg_out = Reg_out;
+			endcase
 		end
 	end
 
